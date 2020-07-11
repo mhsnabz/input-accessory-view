b85e43a6cd8c6e8455f1fb41d4592bb18368c20b
mhsnabz <mhsnabz@yahoo.com.tr>
2020-07-11 09:20:36 +0300

    no msg

---
+++ input accessory view/input accessory view/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  input accessory view
+//
+//  Created by mahsun abuzeyitoğlu on 11.07.2020.
+//  Copyright © 2020 mahsun abuzeyitoğlu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
